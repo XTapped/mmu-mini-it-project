b8b6c92fe42888f5747eac4c91d0f4ce0df1d0ef
Laxman Pillai <laxman.pillai1104@gmail.com>
2023-08-14 22:48:47 +0800

    Add Back Button

---
+++ Back_Button.py
@@ -0,0 +1,29 @@
+from tkinter import*
+from PIL import ImageTk,Image
+
+root=Tk()
+root.title="BackButton"
+root.geometry("200x300")
+
+class BackButton:
+    def __init__(self,back_button):
+        myFrame = Frame(back_button)
+        myFrame.pack()
+        
+        self.my_img = Image.open("back_arrow.png")  
+        self.my_img = self.my_img.resize((50, 50)) 
+        
+        self.my_img = ImageTk.PhotoImage(self.my_img)
+        self.my_label = Label(image = self.my_img)
+
+        self.my_button = Button(back_button, image=self.my_img, command=self.clicker, borderwidth=0)
+        self.my_button.pack()
+        self.my_button.place(x=5, y=10)
+
+    def clicker(self):
+        print("You clicked the image button!") 
+
+
+back = BackButton(root)
+
+root.mainloop()
