26d34aa4a7a695a384440897d5b9a66183479d46
Laxman Pillai <laxman.pillai1104@gmail.com>
2023-08-22 23:25:29 +0800

    Code redesign & Position specifying

--- RedButton.py
+++ RedButton.py
@@ -1,39 +1,40 @@
 from tkinter import *
 
 root = Tk()
-#Create class for whitebutton
+
+#Create class for redbutton
 class RedButton:
-    def __init__(self,root,label,size):
-        self.myFrame = Frame(root)
-        self.myFrame.pack()
-
+    def __init__(self,root,label,size,x_pos, y_pos):
+       
         #Button Border Atribuites
-        self.button_frame = Frame(root,  
-                                 highlightbackground="black", 
-                                 highlightcolor="black", 
-                                 highlightthickness=1)
+        self.button_frame = Frame(
+            root,  
+            bg="black",
+            highlightbackground="black", 
+            highlightcolor="black", 
+            highlightthickness=1
+        )
         
-        self.button_frame.pack(padx=10, pady=10)
-
-
         #Atribuites of button
-        self.red_button = Button(self.button_frame,
-                                  text=label, 
-                                  font="inter",
-                                  bg="red",
-                                  fg="white",
-                                  width=size,
-                                  borderwidth=0, 
-                                  relief="flat",
-                                  activebackground="red",
-                                  activeforeground="black",
-                                  command=self.clicker, )
+        self.red_button = Button(
+            self.button_frame,
+            text=label, 
+            font="inter",
+            command=self.clicker,
+            bg="red",
+            fg="white",
+            width=size,
+            borderwidth=0, 
+            relief="flat",
+            activebackground="red",
+            activeforeground="black")
 
         self.red_button.pack()
+        self.button_frame.place(x=x_pos, y=y_pos)
         
     def clicker(self): 
         pass
 
-#Specifying what text and size should be the button
-rb = RedButton(root,"TEXT","10")
+#Specifying what text size and possition should the button be 
+rb = RedButton(root,"TEXT","10", x_pos=3, y_pos=5)
 root.mainloop()
