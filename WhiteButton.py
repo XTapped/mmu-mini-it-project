96c4ca922e5f89ca99f44d89c563804ee2827578
Laxman Pillai <laxman.pillai1104@gmail.com>
2023-08-22 22:58:31 +0800

    Redesign code & add button position specifying.

--- WhiteButton.py
+++ WhiteButton.py
@@ -1,39 +1,38 @@
 from tkinter import *
 
 root = Tk()
+
 #Create class for whitebutton
 class WhiteButton:
-    def __init__(self,root,label,size):
-        self.myFrame = Frame(root)
-        self.myFrame.pack()
-
-        #Button Border Atribuites
-        self.button_frame = Frame(root, 
-                                 bg="black", 
-                                 highlightbackground="black", 
-                                 highlightcolor="black", 
-                                 highlightthickness=1)
+    def __init__(self,root,label,size,x_pos, y_pos):
         
-        self.button_frame.pack(padx=10, pady=10)
-
+        #Button Border Atribuites
+        self.button_frame = Frame(
+            root,
+            bg="black",
+            highlightbackground="black",
+            highlightcolor="black",
+            highlightthickness=1
+        )
 
         #Atribuites of button
-        self.white_button = Button(self.button_frame,
-                                  text=label, 
-                                  font="inter",
-                                  command=self.clicker, 
-                                  bg="white",
-                                  width=size,
-                                  borderwidth=0,
-                                  fg="black", 
-                                  relief="flat")
-
+        self.white_button = Button(
+            self.button_frame,
+            text=label, 
+            font="inter",
+            command=self.clicker, 
+            bg="white",
+            fg="black",
+            width=size,
+            borderwidth=0,
+            relief="flat")
+                                  
         self.white_button.pack()
+        self.button_frame.place(x=x_pos, y=y_pos)
         
     def clicker(self): 
         pass
 
-
-#Specifying what text and size should be the button
-wb = WhiteButton(root,"TEXT","10")
+#Specifying what text size and possition should the button be 
+wb = WhiteButton(root,"TEXT","10", x_pos=3, y_pos=5)
 root.mainloop()
