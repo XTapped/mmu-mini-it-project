3b5d63b731b4135432842680e9bd1a1669f10c2c
WideTracked <harrismajeed2004@gmail.com>
2023-08-23 16:34:27 +0800

    fix and refactor code
    The code was not working in all cases before and was generally pretty, uh, unidiomatic. So, I made some changes and now it works great.

--- MMULeft.py
+++ MMULeft.py
@@ -1,18 +1,19 @@
-# import tkinter
-from tkinter import * 
+from tkinter import *
+
+
 class MMULeft:
-    def __init__(self):
-        window = Tk()
-        window.title("MMU Center")
-        # create a canvas
-        canvas = Canvas(window, width = 800, height = 500)
-        canvas.pack(padx=48 ,pady=80)
-        canvas.place(x=-30, y=48)
-        # create a photoimage object
-        image1 = PhotoImage(file = "mmuleft.png")
-        # create a label in the canvas
-        canvas.create_image(160, 49, image = image1)
-        # run the mainloop
-        window.mainloop()
-# call the class
-MMULeft()
+    def __init__(self, frame):
+        self._frame = frame
+        self._image = PhotoImage(file="assets/mmuleft.png")
+        self._padding = 40
+
+        self._canvas = Canvas(
+            self._frame, width=self._image.width(), height=self._image.height()
+        )
+        self._canvas.create_image(
+            self._image.width() / 2, self._image.height() / 2, image=self._image
+        )
+
+    def pack(self, **kwargs):
+        self._canvas.pack(pady=self._padding, **kwargs)
+        self._canvas.place(x=48, y=80)
