00f015bc36b85a73077e4a8ddd30a3a7ce3c8060
WideTracked <harrismajeed2004@gmail.com>
2023-08-23 14:35:56 +0800

    improve var names

--- BackButton.py
+++ BackButton.py
@@ -3,15 +3,13 @@ from tkinter import *
 
 class BackButton:
     def __init__(self, root):
-        self.my_img = PhotoImage(file="backarrow.png")
+        self.img = PhotoImage(file="backarrow.png")
 
-        self.my_button = Button(
-            root, image=self.my_img, command=self.clicker, borderwidth=0
-        )
+        self.my_button = Button(root, image=self.img, command=self.back, borderwidth=0)
 
     def pack(self):
         self.my_button.pack()
         self.my_button.place(x=5, y=10)
 
-    def clicker(self):
+    def back(self):
         pass
