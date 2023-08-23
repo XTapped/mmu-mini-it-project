e6af4ee8a609196b27a2a96f58601518165c68d4
WideTracked <harrismajeed2004@gmail.com>
2023-08-23 12:41:06 +0800

    fix formatting

--- BackButton.py
+++ BackButton.py
@@ -1,21 +1,24 @@
-from tkinter import*
-from PIL import ImageTk,Image
+from tkinter import *
+from PIL import ImageTk, Image
 
-root=Tk()
-root.title="BackButton"
+root = Tk()
+root.title = "BackButton"
 root.geometry("200x300")
 
+
 class BackButton:
-    def __init__(self,back_button):
+    def __init__(self, back_button):
         myFrame = Frame(back_button)
         myFrame.pack()
-        
-        self.my_img = Image.open("backarrow.png")  
-         
-        self.my_img = ImageTk.PhotoImage(self.my_img)
-        self.my_label = Label(image = self.my_img)
 
-        self.my_button = Button(back_button, image=self.my_img, command=self.clicker, borderwidth=0)
+        self.my_img = Image.open("backarrow.png")
+
+        self.my_img = ImageTk.PhotoImage(self.my_img)
+        self.my_label = Label(image=self.my_img)
+
+        self.my_button = Button(
+            back_button, image=self.my_img, command=self.clicker, borderwidth=0
+        )
         self.my_button.pack()
         self.my_button.place(x=5, y=10)
 
