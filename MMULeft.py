1e247285d8aa3dbec1d82c88eaf8a370fd498c27
WideTracked <harrismajeed2004@gmail.com>
2023-08-25 17:29:17 +0800

    majorly refactor MMULeft

--- MMULeft.py
+++ MMULeft.py
@@ -1,19 +1,39 @@
 from tkinter import *
+from typing import *
 
 
-class MMULeft:
-    def __init__(self, frame):
-        self._frame = frame
+class MMUCenter(Canvas):
+    """
+    A picture of the MMU logo centered in the middle of the screen. This class inherits from tk.Canvas.
+
+    Args:
+        root (Tk): The root window.
+    """
+
+    def __init__(self, root: Tk):
         self._image = PhotoImage(file="assets/mmuleft.png")
-        self._padding = 40
+        super().__init__(root, width=self._image.width(), height=self._image.height())
 
-        self._canvas = Canvas(
-            self._frame, width=self._image.width(), height=self._image.height()
-        )
-        self._canvas.create_image(
+        self.create_image(
             self._image.width() / 2, self._image.height() / 2, image=self._image
         )
 
-    def pack(self, **kwargs):
-        self._canvas.pack(pady=self._padding, **kwargs)
-        self._canvas.place(x=48, y=80)
+    def pack(self):
+        super().pack()
+        super().place(x=48, y=80)
+
+
+# TEST CODE
+# if __name__ == "__main__":
+#     root = Tk()
+#     root.geometry("800x500")
+#     root.title("MMUCenter Test")
+#     root.resizable(False, False)
+
+#     frame = Frame(root, width=800, height=500)
+#     frame.pack()
+
+#     mmu_center = MMUCenter(frame)
+#     mmu_center.pack()
+
+#     root.mainloop()
