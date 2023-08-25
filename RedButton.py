ea0fed4e8b9cff20a959610a8ee960ef8198f467
WideTracked <harrismajeed2004@gmail.com>
2023-08-25 16:17:00 +0800

    refactor everything

--- RedButton.py
+++ RedButton.py
@@ -1,40 +1,41 @@
 from tkinter import *
+from typing import *
 
-root = Tk()
 
-#Create class for redbutton
-class RedButton:
-    def __init__(self,root,label,size,x_pos, y_pos):
-       
-        #Button Border Atribuites
-        self.button_frame = Frame(
-            root,  
+class RedButton(Frame):
+    """
+    A red button with white text. This class extends tk.Frame()
+
+    Args:
+        root (Tk): The root window.
+        text (str): The text to display on the button.
+        command (Optional[Callable], optional): The function to call when the button is clicked. Defaults to None.
+        width (int, optional): The width of the button. Defaults to 10.
+    """
+
+    def __init__(
+        self, root: Tk, text: str, command: Optional[Callable] = None, width: int = 10
+    ):
+        super().__init__(
+            root,
             bg="black",
-            highlightbackground="black", 
-            highlightcolor="black", 
-            highlightthickness=1
+            highlightbackground="black",
+            highlightcolor="black",
+            highlightthickness=1,
         )
-        
-        #Atribuites of button
+
         self.red_button = Button(
-            self.button_frame,
-            text=label, 
+            self,
+            text=text,
             font="inter",
-            command=self.clicker,
+            command=command,
             bg="red",
             fg="white",
-            width=size,
-            borderwidth=0, 
+            width=width,
+            borderwidth=0,
             relief="flat",
             activebackground="red",
-            activeforeground="black")
+            activeforeground="black",
+        )
 
         self.red_button.pack()
-        self.button_frame.place(x=x_pos, y=y_pos)
-        
-    def clicker(self): 
-        pass
-
-#Specifying what text size and possition should the button be 
-rb = RedButton(root,"TEXT","10", x_pos=3, y_pos=5)
-root.mainloop()
