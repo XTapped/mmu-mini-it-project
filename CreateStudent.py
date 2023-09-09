90ceb82caa8b02b5b4ad00978e9ad762206df41b
Harris Majeed <harrismajeed2004@gmail.com>
2023-09-09 15:23:52 +0800

    make page scrollable

--- CreateStudent.py
+++ CreateStudent.py
@@ -5,9 +5,10 @@ from modules import BackButton
 from modules import MMULeft
 from modules import TextEntry
 from modules import WhiteButton
+from modules import ScrollableFrame
 
 
-class CreateStudent:
+class CreateStudent(ScrollableFrame):
 
     """
     A page for creating a student.
@@ -17,30 +18,30 @@ class CreateStudent:
     """
 
     def __init__(self, root):
-        self.root = root
+        super().__init__(root, height=800)
 
-        self.root.geometry("800x617")  # Set window dimensions
-
-        self._mmu_left = MMULeft(self.root)
+        self._mmu_left = MMULeft(self.interior)
         self._mmu_left.pack()  # Placing the MMULeft logo first
 
-        self._back_button = BackButton(self.root, None)
+        self._back_button = BackButton(self.interior, None)
         self._back_button.pack()  # Placing the BackButton on top of MMULeft
         self._back_button.place(x=48, y=40)
 
-        self._student_name_entry = TextEntry(self.root, "Student Name", width=38)
+        self._student_name_entry = TextEntry(self.interior, "Student Name", width=38)
         self._student_id_entry = TextEntry(
-            self.root, "Student ID", regen_student_id=False, copy=True, width=38
+            self.interior, "Student ID", regen_student_id=False, copy=True, width=38
         )
         self._student_password_entry = TextEntry(
-            self.root, "Student Password", regen_password=True, copy=True, width=38
+            self.interior, "Student Password", regen_password=True, copy=True, width=38
         )
         self._current_semester_entry = TextEntry(
-            self.root, "Current Semester", width=38
+            self.interior, "Current Semester", width=38
         )
-        self._current_program_entry = TextEntry(self.root, "Current Program", width=38)
+        self._current_program_entry = TextEntry(
+            self.interior, "Current Program", width=38
+        )
         self._create_button = WhiteButton(
-            self.root,
+            self.interior,
             "Create Student",
             self._create_student_handler,
             width=14,
@@ -61,11 +62,11 @@ class CreateStudent:
         self._create_button.place(x=48, y=610)
 
     def _create_student_handler(self):
-        student_name = self._student_name_entry.get_text()
-        student_id = self._student_id_entry.get_text()
-        student_password = self._student_password_entry.get_password()
-        current_semester = self._current_semester_entry.get_text()
-        current_program = self._current_program_entry.get_text()
+        student_name = self._student_name_entry.get()
+        student_id = self._student_id_entry.get()
+        student_password = self._student_password_entry.get()
+        current_semester = self._current_semester_entry.get()
+        current_program = self._current_program_entry.get()
 
         # Here, you would perform the actual student creation logic
         # For now, let's just print the student details
@@ -78,11 +79,12 @@ class CreateStudent:
             f"Program: {current_program}"
         )
 
-#TEST CODE 
-# if __name__ == "__main__":
-#     import tkinter as tk
 
-#     root = tk.Tk()
-#     root.title("Create Student")
-#     create_student_page = CreateStudent(root)
-#     root.mainloop()
+# TEST CODE
+if __name__ == "__main__":
+    import tkinter as tk
+
+    root = tk.Tk()
+    root.title("Create Student")
+    create_student_page = CreateStudent(root)
+    root.mainloop()
