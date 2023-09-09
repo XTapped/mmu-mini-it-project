acc67773a559d9c362dd38c1f82565259df10232
Harris Majeed <harrismajeed2004@gmail.com>
2023-09-09 15:14:34 +0800

    enclose test code in if name main

--- SplashScreen.py
+++ SplashScreen.py
@@ -1,4 +1,8 @@
-from modules import MMUCenter, Heading, TextEntry, PasswordEntry, WhiteButton
+from modules import MMUCenter
+from modules import Heading
+from modules import TextEntry
+from modules import PasswordEntry
+from modules import WhiteButton
 import tkinter as tk
 
 
@@ -9,14 +13,17 @@ class SplashScreen(tk.Frame):
         content_frame = tk.Frame(root)
         content_frame.pack()
         content_frame.place(y=150, x=170)
-        heading = Heading(content_frame , "Course Registration System", 3).pack(pady=(0,23))
-        student_id = TextEntry(content_frame, "Student ID", width=35).pack(pady=(0,23))
-        password = PasswordEntry(content_frame, width=35).pack(pady=(0,40)) 
-        sign_in_button = WhiteButton(content_frame, "Sign In", None).pack(pady=(0,23))
+        heading = Heading(content_frame, "Course Registration System", 3).pack(
+            pady=(0, 23)
+        )
+        student_id = TextEntry(content_frame, "Student ID", width=35).pack(pady=(0, 23))
+        password = PasswordEntry(content_frame, width=35).pack(pady=(0, 40))
+        sign_in_button = WhiteButton(content_frame, "Sign In", None).pack(pady=(0, 23))
 
 
-master = tk.Tk()
-master.geometry("800x500")
-master.resizable(False, False)
-SplashScreen(master).pack()
-master.mainloop()
+if __name__ == "__main__":
+    master = tk.Tk()
+    master.geometry("800x500")
+    master.resizable(False, False)
+    SplashScreen(master).pack()
+    master.mainloop()
