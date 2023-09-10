cbf59ee6eaab276f26eb3dd258aac69048b0dad8
Harris Majeed <harrismajeed2004@gmail.com>
2023-09-11 00:04:35 +0800

    wip updatestudent before any deletion logic

--- UpdateStudent.py
+++ UpdateStudent.py
@@ -1,129 +1,82 @@
-
 import tkinter as tk
-from typing import *
-from modules import BackButton, MMULeft, WhiteButton, Heading, RedButton
-from modules import ScrollableFrame
+from idlelib.tooltip import Hovertip
+from modules import (
+    ScrollableFrame,
+    BackButton,
+    MMULeft,
+    Heading,
+    WhiteButton,
+    RedButton,
+)
 
 
-
-class CoursesMenu(ScrollableFrame):
-    def init(self, root: tk.Tk):
-        total_height = 10 * 10
-        super().init(root, "Courses Menu", height=total_height)
-        
-
-class UpdateStudent(tk.Frame):
-    def __init__(self, root):   
+class UpdateStudent(ScrollableFrame):
+    # TODO: Remove all the Nones from the params
+    def __init__(self, root, back_frame=None):
         super().__init__(root)
-        self.config(width=800, height=500)
-        
-        
+        self._back_button = BackButton(self.interior, back_frame)
+        self._back_button.pack()
 
-        # Main frame
-        main_frame = tk.Frame(self)
-        main_frame.pack(pady=1)  # Added vertical padding
+        self._mmu_logo = MMULeft(self.interior)
+        self._mmu_logo.pack()
 
-        # BackButton
-        back_frame = tk.Frame(main_frame)  # Changed root to self
-        self.back_button = BackButton(
-            root=root, back_frame=back_frame
-        )  # Corrected root=tk to root=root
-        self.back_button.pack()
-        back_frame.pack()  # Changed grid to pack
+        self._student_container = tk.Frame(self.interior)
+        self._student_container.pack(fill="x", pady=180, padx=50)
 
-        # MMULeft logo
-        mmu_frame = tk.Frame(main_frame)  # Add a frame for MMULeft
-        self.mmu_logo = MMULeft(
-            root=root
-        )  # Corrected root to root and removed parent argument
-        self.mmu_logo.pack()
-        mmu_frame.pack()  # Changed grid to pack
-
-        self._Avatar = tk.PhotoImage(file="assets/Avatar.png")
-        self._AvatarLabel = tk.Label(main_frame, image=self._Avatar, compound=tk.LEFT)
-        # Students
-        self.students_frame = tk.Frame(main_frame)  # Changed root to self
-        self.students_data = [
-            ("Kubenthran Udayar", self.on_update, self.on_delete),
-            ("Hawash Abdullah", self.on_update, self.on_delete),
-            ("Student 3", self.on_update, self.on_delete),
-            ("Student 4", self.on_update, self.on_delete),
-            ("Student 5", self.on_update, self.on_delete),
-            ("Student 6", self.on_update, self.on_delete),
+        self._student_list = [
+            "Harris Majeed",
+            "Vinesh Sivaneswaran",
+            "Laxman Pillai",
+            "Kubenthran Udayar",
+            "Khiisyen Nair",
         ]
 
-        self.student_frames = []
+        self._column_num = 1
+        for index, student in enumerate(self._student_list):
+            if index % 2 == 0:
+                self._row_num = index // 2
+            self._column_num = 0 if self._column_num == 1 else 1
 
-        for i, student_data in enumerate(self.students_data):
-            student_frame = tk.Frame(self.students_frame)
-            self.student_frames.append(student_frame)
-            student_name = student_data[0]
-            update_button = WhiteButton(
-                student_frame, "Update", student_data[1], width=12, height=2
+            _Student(self._student_container, student).grid(
+                row=self._row_num, column=self._column_num, padx=(0, 50), pady=(0, 50)
             )
 
-            delete_button = RedButton(
-                student_frame,
-                "Delete",
-                lambda i=i: self.on_delete(i),
-                width=12,
-                height=2,
-            )
 
-            heading = Heading(student_frame, student_name, 4)
-            heading.grid(
-                row=0, column=1, columnspan=1
-            )  # Added vertical padding
-            heading.place(x=150, y=6)
+class _Student(tk.Frame):
+    def __init__(self, root, name):
+        super().__init__(root)
 
-            student_label = tk.Label(
-                student_frame, image=self._Avatar, compound=tk.LEFT
-            )
+        self.list_pos = list_pos
 
-            update_button.grid(row=1, column=1, padx=8)  # Added horizontal padding
-            delete_button.grid(
-                row=1, column=2, padx=8, pady=45
-             )  # Added horizontal padding
-            student_label.grid(row=1, column=0, padx=0 )  # Added padding
+        self._avatar_frame = tk.Frame(self)
+        self._avatar_frame.pack(side="left")
 
-            # Calculate new row and column values
-            row = (i + 2) // 2  # Start from row 2 instead of row 0
-            column = (i + 2) % 2  # Alternate between column 0 and column 1
+        self._text_frame = tk.Frame(self)
+        self._text_frame.pack(padx=10)
 
-            # Set different padx values for different columns
-            if column == 0:
-                padx = 25
-            else:
-                padx = 350
+        self._avatar_img = tk.PhotoImage(file="assets/avatar.png")
+        self._avatar = tk.Label(self._avatar_frame, image=self._avatar_img)
+        self._avatar.pack()
 
-            student_frame.grid(
-                row=row, column=column, padx=padx, pady=10
-            )  # Increased padding
+        self._student_name = Heading(self._text_frame, self._truncate_string(name), 4)
+        self._student_name.pack(anchor="w")
 
-        self.students_frame.pack(
-            side="left", pady=10
-        )  # Kept pack to ensure it's below the MMULeft logo
-        self.pack(side="left", padx=10, pady=120)
+        self._name_tooltip = Hovertip(self._student_name, name, hover_delay=200)
 
-    def on_update(self):
-        """Handle update logic here"""
-        pass
+        self._update_button = WhiteButton(self._text_frame, "Update", None, 8, 2)
+        self._update_button.pack(side="left", anchor="w", padx=5)
 
-    def on_delete(self, index):
-        """Remove the student at the given index"""
-        # Remove the student's data
-        del self.students_data[index]
-        # Remove the student's frame from the GUI
-        self.student_frames[index].grid_forget()
-        del self.student_frames[index]
+        self._delete_button = RedButton(self._text_frame, "Delete", None, 8, 2)
+        self._delete_button.pack(side="left", anchor="w", padx=5)
 
+    def _truncate_string(self, string):
+        if len(string) > 13:
+            return string[:10] + "..."
+        else:
+            return string
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("800x500")
-    root.title("Update Student Page")
-    update_student_page = UpdateStudent(root)
-
-    # Added vertical padding to ensure it's below the MMULeft logo
+    UpdateStudent(root).pack()
     root.mainloop()
