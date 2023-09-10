b4bbdc5171b3241c2a963f77c2fe455c377ad8e1
Harris Majeed <harrismajeed2004@gmail.com>
2023-09-11 00:26:38 +0800

    initial implementation: updatestudent

--- UpdateStudent.py
+++ UpdateStudent.py
@@ -30,6 +30,7 @@ class UpdateStudent(ScrollableFrame):
             "Kubenthran Udayar",
             "Khiisyen Nair",
         ]
+        self._student_widget_list = []
 
         self._column_num = 1
         for index, student in enumerate(self._student_list):
@@ -37,16 +38,39 @@ class UpdateStudent(ScrollableFrame):
                 self._row_num = index // 2
             self._column_num = 0 if self._column_num == 1 else 1
 
-            _Student(self._student_container, student).grid(
+            widget = _Student(self._student_container, student, index, self.regrid)
+            widget.grid(
                 row=self._row_num, column=self._column_num, padx=(0, 50), pady=(0, 50)
             )
+            self._student_widget_list.append(widget)
+
+    def regrid(self, list_pos):
+        self._student_list.pop(list_pos)
+
+        for widget in self._student_widget_list:
+            widget.destroy()
+
+        self._student_widget_list.clear()
+
+        self._column_num = 1
+        for index, student in enumerate(self._student_list):
+            if index % 2 == 0:
+                self._row_num = index // 2
+            self._column_num = 0 if self._column_num == 1 else 1
+
+            widget = _Student(self._student_container, student, index, self.regrid)
+            widget.grid(
+                row=self._row_num, column=self._column_num, padx=(0, 50), pady=(0, 50)
+            )
+            self._student_widget_list.append(widget)
 
 
 class _Student(tk.Frame):
-    def __init__(self, root, name):
+    def __init__(self, root, name, list_pos, delete_func):
         super().__init__(root)
 
         self.list_pos = list_pos
+        self._delete_func = delete_func
 
         self._avatar_frame = tk.Frame(self)
         self._avatar_frame.pack(side="left")
@@ -66,7 +90,9 @@ class _Student(tk.Frame):
         self._update_button = WhiteButton(self._text_frame, "Update", None, 8, 2)
         self._update_button.pack(side="left", anchor="w", padx=5)
 
-        self._delete_button = RedButton(self._text_frame, "Delete", None, 8, 2)
+        self._delete_button = RedButton(
+            self._text_frame, "Delete", self._super_destroy, 8, 2
+        )
         self._delete_button.pack(side="left", anchor="w", padx=5)
 
     def _truncate_string(self, string):
@@ -74,6 +100,9 @@ class _Student(tk.Frame):
             return string[:10] + "..."
         else:
             return string
+
+    def _super_destroy(self):
+        self._delete_func(self.list_pos)
 
 
 if __name__ == "__main__":
