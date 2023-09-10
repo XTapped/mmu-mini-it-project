36640c783d7cc6df3836e5deddf93baf86669789
Laxman Pillai <laxman.pillai1104@gmail.com>
2023-09-10 22:26:53 +0800

    Did some formating

--- AdminMenu.py
+++ AdminMenu.py
@@ -1,4 +1,4 @@
-import tkinter as tk 
+import tkinter as tk
 
 from modules.BackButton import BackButton
 from modules.MMULeft import MMULeft
@@ -6,50 +6,40 @@ from modules.WhiteButton import WhiteButton
 from modules.Heading import Heading
 
 
-root=tk.Tk()
+root = tk.Tk()
+
 
 class AdminMenu(tk.Frame):
     def __init__(
         self,
-        root:tk.Tk,
+        root: tk.Tk,
     ):
-        
         root.geometry("800x500")
         root.title("Admin Menu")
-        root.resizable(False,False)
+        root.resizable(False, False)
         super().__init__(root)
 
-    
-        self.backbutton = BackButton(root,None).pack()
-    
+        self.backbutton = BackButton(root, None).pack()
+
         self.mmuleft = MMULeft(root).pack()
 
-        self.student_name_label = Heading(
-            root,
-            "Admin",
-            3,
-            "#0650A4"
-        )
-        self.student_name_label.place(x=140,y=149)      
+        self.student_name_label = Heading(root, "Admin", 3, "#0650A4")
+        self.student_name_label.place(x=140, y=149)
 
-        self.welcome_heading = Heading(root,"Hello,",3)
-        self.welcome_heading.place(x=48,y=149)
-        
+        self.welcome_heading = Heading(root, "Hello,", 3)
+        self.welcome_heading.place(x=48, y=149)
 
+        self.description_heading = Heading(root, "What would you like to do today?", 3)
+        self.description_heading.place(x=48, y=190)
 
-        self.description_heading = Heading(root,"What would you like to do today?",3)
-        self.description_heading.place(x=48,y=190)
+        self.student_action = WhiteButton(root, "Student Actions", None, 20, 2)
+        self.student_action.place(x=48, y=245)
 
+        self.course_action = WhiteButton(root, "Course Action", None, 20, 2)
+        self.course_action.place(x=48, y=330)
 
-        self.student_action = WhiteButton(root,"Student Actions",None,20,2)
-        self.student_action.place(x=48,y=245)
-
-        self.course_action = WhiteButton(root, "Course Action",None,20,2)
-        self.course_action.place(x=48,y=330)
-
-    
 
 student_menu = AdminMenu(root)
 student_menu.pack()
-    
+
 root.mainloop()
